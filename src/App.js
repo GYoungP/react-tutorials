06b93cb64b3135cae5da07c8372407d0f2167c9f
gyoung95 <gyoung95@nmp-corp.com>
2022-09-27 17:33:30 +0900

    배열에 데이터 삽입하기

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import PhoneForm from './components/PhoneForm';
 
 
 class App extends Component{
+    id = 0;
 
     state = {
         information: [],
     }
 
     handleCreate = (data) => {
-        console.log(data);
+        const { information } = this.state
+        this.setState({
+            information: information.concat({
+                ...data,
+                id: this.id++
+            }),
+        })
     }
 
     render() {
         return (
             <div>
                 <PhoneForm onCreate={this.handleCreate} />
+                {JSON.stringify(this.state.information)}
             </div>
         )
     }
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -16,6 +16,10 @@ class PhoneForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.onCreate(this.state);
+        this.setState({
+            name:'',
+            phone:'',
+        })
     }
 
     render() {
@@ -24,9 +28,6 @@ class PhoneForm extends Component {
                 <input name='name' placeholder='이름' onChange={this.handleChange} value={this.state.name}/>
                 <input name='phone' placeholder='전화번호' onChange={this.handleChange} value={this.state.phone}/>
                 <button type="submit">등록</button>
-                <div>
-                {this.state.name} {this.state.phone}
-                </div>
             </form>
         );
     }
